aa06ecb26db3eba6dc9e33bd57ce32ff25aeebe1
LEO STRIETZEL <leostrietzel@gmail.com>
2024-12-24 23:30:24 +0100

    Initial Commit

---
+++ HUDNAV/HUDNAVApp.swift
@@ -0,0 +1,17 @@
+//
+//  HUDNAVApp.swift
+//  HUDNAV
+//
+//  Created by Leo Strietzel on 24.12.24.
+//
+
+import SwiftUI
+
+@main
+struct HUDNAVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
